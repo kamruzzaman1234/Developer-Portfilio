86540bf4542edeeaecb1e3206a02915d149039b3
Kamruzzaman <kamruzzamancse22@gmail.com>
2025-06-30 12:54:08 +0800

    work card code added

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../../components/About/About";
 import Choose from "../../components/Choose/Choose";
 import Headers from "../../components/Headers/Headers";
+import Work from "../../components/Work/Work";
 
 const Home = ()=>{
     return(
@@ -8,6 +9,7 @@ const Home = ()=>{
             <Headers></Headers>
             <Choose></Choose>
             <About></About>
+            <Work></Work>
         </div>
     )
 }
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -4,12 +4,88 @@ const Work = ()=>{
             <div className="py-[80px] bg-[#141414] rounded-lg">
                 <div className="max-w-[1410px] w-full mx-auto">
                     <div className="flex flex-col gap-[50px]">
-                        <div className="flex gap-[97px]">
+                        <div className="flex gap-[97px] items-center">
                             <div className="w-[175px]">
                                 <img src="https://i.ibb.co/2791L74s/Group-26.png" className="w-full" alt="" />
                             </div>
                             <div>
                                 <h2 className="text-[58px] leading-[86px] font-bold">My Work Process</h2>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-[10px]">
+                            <div className="p-[30px] flex flex-col gap-[45px] bg-black rounded-lg">
+                                <div className="flex justify-between">
+                                        <div className="bg-[#C5FFEE] text-black
+                                         rounded-full px-[35px] py-[2px]">
+                                            <h4 className="text-[20px] leading-[34px] leading-[40px]">Discovery</h4>
+                                        </div>
+                                    <div className="text-sm flex items-center gap-2">
+                                      <span className="text-white">→</span>
+                                       <a href="#" className="underline text-sm">
+                                         Read More
+                                       </a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-[18px] font-regular text-[#ffffff]">We start every new client interaction with an in-depth discovery call where we get to know each other,
+                                     discuss your current and future objectives, and recommend the best course of action.</p>
+                                </div>
+                            </div>
+                              <div className="p-[30px] flex flex-col gap-[45px] bg-black rounded-lg">
+                                <div className="flex justify-between">
+                                        <div className="bg-[#C5FFEE] text-black
+                                         rounded-full px-[35px] py-[2px]">
+                                            <h4 className="text-[20px] leading-[34px] leading-[40px]">Strategy</h4>
+                                        </div>
+                                    <div className="text-sm flex items-center gap-2">
+                                      <span className="text-white">→</span>
+                                       <a href="#" className="underline text-sm">
+                                         Read More
+                                       </a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-[18px] font-regular text-[#ffffff]">Every end-to-end project of ours begins with a tEspoke pre-build strateu. 
+                                    From brand ID consultation to in-depth ccxle reviews we're here to set the stage for success.</p>
+                                </div>
+                            </div>
+                              <div className="p-[30px] flex flex-col gap-[45px] bg-black rounded-lg">
+                                <div className="flex justify-between">
+                                        <div className="bg-[#C5FFEE] text-black
+                                         rounded-full px-[35px] py-[2px]">
+                                            <h4 className="text-[20px] leading-[34px] leading-[40px]">Design</h4>
+                                        </div>
+                                    <div className="text-sm flex items-center gap-2">
+                                      <span className="text-white">→</span>
+                                       <a href="#" className="underline text-sm">
+                                         Read More
+                                       </a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-[18px] font-regular text-[#ffffff]">After we have a comprehensive understanding of your brand, 
+                                        we'll be ready to move onto design.
+                                         Each page or will be designed, reviewed, and given your stamp of approval.</p>
+                                </div>
+                            </div>
+                              <div className="p-[30px] flex flex-col gap-[45px] bg-black rounded-lg">
+                                <div className="flex justify-between">
+                                        <div className="bg-[#C5FFEE] text-black
+                                         rounded-full px-[35px] py-[2px]">
+                                            <h4 className="text-[20px] leading-[34px] leading-[40px]">Bluid</h4>
+                                        </div>
+                                    <div className="text-sm flex items-center gap-2">
+                                      <span className="text-white">→</span>
+                                       <a href="#" className="underline text-sm">
+                                         Read More
+                                       </a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-[18px] font-regular text-[#ffffff]">Whether we've just finished designing your new site or
+                                         you're handing off finished designs for us to develop in Webflow,
+                                         we're here to apply our trusted development process to your project.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
